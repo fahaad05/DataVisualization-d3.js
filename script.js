a661325bff764da69d681e37ced3c60c0fb593ba
Fahaad <fahaadahmed05@gmail.com>
2020-02-01 13:34:37 +0100

    commit on "master"

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Load Csv File
+
+var data;
+var countries = [];
+
+// Load Csv Data File
 d3.csv("data/earn_ses_monthly_1_Data.csv", function (error, csv) {
+    
     if (error) { 
         console.log(error);  //Log the error.
 	throw error;
     }
-
+    
     csv.forEach(function (d) {
-
         sex= d.SEX;
         geo= d.GEO;
         nace= d.NACE_R2;
@@ -16,8 +20,33 @@ d3.csv("data/earn_ses_monthly_1_Data.csv", function (error, csv) {
         time= d.TIME;
         indic_se= d.INDIC_SE;
         value= d.Value;
-
-        console.log(csv);    
     })
     
+    data = csv;    
+
+    data.forEach(function(d) {
+        if (!countries.includes(d.GEO)) 
+            countries.push(d.GEO);
+    })
+    // console.log(countries);
+    radioList();
 });
+
+function radioList() {
+    
+        
+    d3.select("#countriesList").selectAll("div")
+        .data(countries)
+        .enter()
+        .append("div")
+        .attr("id", function(d) {
+            return d;
+        })
+        .select("input").attr("type", "radio").attr("id", function (d){return d}).attr("name", "countriesList").attr("class", "custom-control-input");
+
+}
+
+
+
+
+
